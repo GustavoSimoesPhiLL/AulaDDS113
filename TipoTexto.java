5417b2fe625b8ed97e13994bd1cb0ccc81c76946
PHIKILL_A2 <101225369+GustavoSimoesPhiLL@users.noreply.github.com>
2022-03-10 19:15:27 -0300

    Add files via upload

---
+++ TipoTexto.java
@@ -0,0 +1,17 @@
+package AULADDS113;
+
+import java.util.Scanner;
+
+public class TipoTexto {
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		String nome = scanner.nextLine();
+		
+		System.out.println("Ola    " + nome + "  !");
+		
+		scanner.close();
+	}
+
+}
