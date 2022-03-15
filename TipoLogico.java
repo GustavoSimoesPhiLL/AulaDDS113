7831115289b0e89211a11def082e6e1191cdc46f
PHIKILL_A2 <101225369+GustavoSimoesPhiLL@users.noreply.github.com>
2022-03-15 19:03:06 -0300

    Add files via upload

--- TipoLogico.java
+++ TipoLogico.java
@@ -1,13 +1,27 @@
-package AULADDS113;
+package AlunoBurroQI99999;
 
 public class TipoLogico {
-	
-	public static void main(String[] args ) {
-		Boolean variavelVerdadeira = true;
-		System.out.println("Variavel verdadeira" + variavelVerdadeira);
-		
-		Boolean variavelFalsa = false;
-		System.out.println("Variavel falsa" + variavelFalsa);
-	}
 
+    public static void main(String[] args) {
+        Boolean variavelVerdadeira = true;
+        System.out.println("Variavel verdadeira: " + variavelVerdadeira);
+
+        Boolean variavelFalsa = false;
+        System.out.println("Variavel falsa: " + variavelFalsa);
+        
+        System.out.println("--------------------------------");
+        
+        Integer idade = 17;
+        
+        Boolean podeTirarCarteira = idade>= 18;
+        
+       // System.out.println(" Pode tirar a carteira ? " + podeTirarCarteira);
+        
+       if (podeTirarCarteira) {
+    	   System.out.println("Sim! Ele(a) pode tirar a carteira. ");
+       }else {
+    	   System.out.println("Nao Ele(a) nao pode tirar carteira");
+       }
+        
+    }
 }
